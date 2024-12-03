fae804298c9291cc95068eadbc5b0497183923af
whosyourdaddy <56509282+sawPhoenix@users.noreply.github.com>
2024-12-03 22:15:37 +0800

    feat: add<beginning of the code>  --no-sandbox

--- pages/Note/index.tsx
+++ pages/Note/index.tsx
@@ -46,6 +46,21 @@ const Daily: React.FC = () => {
         {}
         {/* <MarkDownRender>{ReactMD({ components })}</MarkDownRender> */}
       </div>
+      {() => {
+        const quickSort = (arr: number[]): number[] => {
+          if (arr.length <= 1) return arr;
+          const pivot = arr[0];
+          const left: number[] = [];
+          const right: number[] = [];
+          for (let i = 1; i < arr.length; i++) {
+            if (arr[i] < pivot) {
+              left.push(arr[i]);
+            } else {
+              right.push(arr[i]);
+            }
+          }
+        };
+      }}
     </div>
   );
 };
